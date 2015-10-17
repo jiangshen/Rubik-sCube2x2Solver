252ee341518fcf437362fbe8cc65ae8d92e7417c
jiangshen <jiangshen16@icloud.com>
2015-10-17 19:19:54 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/jiangshen/rubikscube2x2solver/CameraMain.java
@@ -0,0 +1,13 @@
+package com.example.jiangshen.rubikscube2x2solver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_main);
+    }
+}
