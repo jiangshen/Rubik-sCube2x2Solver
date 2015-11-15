f646c64988a4f9a67af8f54821b3251faf07ead5
jiangshen <jiangshen16@icloud.com>
2015-11-15 15:22:53 -0500

    added toolbar and about button

--- app/src/main/java/com/example/jiangshen/rubikscube2x2solver/CameraMain.java
+++ app/src/main/java/com/example/jiangshen/rubikscube2x2solver/CameraMain.java
@@ -1,10 +1,16 @@
 package com.example.jiangshen.rubikscube2x2solver;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,6 +23,10 @@ public class CameraMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_main);
+
+        // Set a toolbar to replace the action bar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
         rootView = findViewById(R.id.root_view);
@@ -51,6 +61,35 @@ public class CameraMain extends AppCompatActivity {
         int left = verticalLine.getLeft();
         ImageProcessor.setDimension((int) (box.getX() - squareFrameLayout.getX()), (int) (box.getY() - squareFrameLayout.getY()),
                 horizontalLine.getWidth(), verticalLine.getHeight());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("About");
+            alertDialog.setMessage(String.format("Created at GT Appathon 2015\n\nThis is a simple app to solve a 2 x 2 Rubrik's Cube :)"));
+            alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            alertDialog.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 //    @Override
